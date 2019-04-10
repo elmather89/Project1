5bafc3871dbd5cf8ecbe3d10f0193ef85ee73a71
Sansevieria <elizabethleighmather@gmail.com>
2019-04-10 00:34:39 -0500

    added links for jquery, js, firebase to index.html. added id's to input fields

--- liz.js
+++ liz.js
@@ -2,16 +2,52 @@ $(document).ready(function () {
 
     // global variables
 
-    //configure firebase
-    // ...
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyB4VyTmaU_5FIQNcfpY0OcZxAdCb4ZuOZg",
+        authDomain: "project1-b74ea.firebaseapp.com",
+        databaseURL: "https://project1-b74ea.firebaseio.com",
+        projectId: "project1-b74ea",
+        storageBucket: "project1-b74ea.appspot.com",
+        messagingSenderId: "1097252927851"
+    };
+    firebase.initializeApp(config);
 
     // geolocation === Tasneem
       // need geolocation to show lat & long response in 2 divs
-      // ...
-      // geolocation end    
+      // ... 
 
-    // on-click event for submit button
-    // ...
+    // on-click event for first 2 trail btn clicks
+      // stores user's name
+      // stores user's 2 clicks's values to Firebase
+      // run stateThree();
+      $("#submit").on("click", function(event) {
+        event.preventDefault();
+
+        // get value from input fields
+        var userName = $("#user-name").val().trim();
+        var userLast = $("#user-last-name").val().trim();
+        var userLocale = $("#user-locale").val().trim();
+        var userEmail = $("#user-email").val().trim();
+
+        // user object
+        var addUser = {
+            user: userName,
+            last: userLast,
+            locale: userLocale,
+            email: userEmail
+        };
+        console.log(addUser.user);
+
+        // push to Firebase
+        database.ref().push(addUser);
+
+        // clear the input fields
+        $("#user-name").val("");
+        $("#user-locale").val("");
+        $("#user-email").val("");
+
+    });
 
     // remove form 
     // ...
@@ -37,6 +73,9 @@ $(document).ready(function () {
 
     // ajax call -- search hiking api function
       // takes vars lat & long, searches hiking api, passes data back up to hiking buttons func
+      var lat = "";
+      var lon = "";
+
       var searchTrails = function(lat, lon) {
           var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=10&key=200444715-18e2274b2d33b9a8db21c47ddfab5855";
 
@@ -48,49 +87,20 @@ $(document).ready(function () {
           });
       };
 
-    // call search function (to test that response is working)
-    searchTrails();
-
     // hiking api end ===============================================
-
-    // on-click event for first 2 trail btn clicks
-      // stores user's name
-      // stores user's 2 clicks's values to Firebase
-      // run stateThree();
-      $("#submit").on("click", function(event) {
-          event.preventDefault();
-
-          // get value from input fields
-          var userName = $("#user-name").val().trim();
-          var userLocale = $("#user-locale").val().trim();
-          var userEmail = $("#user-email").val().trim();
-
-          // user object
-          var addUser = {
-              newUser: userName,
-              newLocale: userLocale,
-              newEmail: userEmail
-          };
-
-          // push to Firebase
-          database.ref().push(addUser);
-
-          // clear the input fields
-          $("#user-name").val("");
-          $("#user-locale").val("");
-          $("#user-email").val("");
-
-      });
-
-      database.ref().on("child_added", function(childSnapshot) {
-          var newUser = childSnapshot.val().userName;
-          var 
-      });
 
     // stateThree();
     // retrieve Firebase data for current user
     // remove generated tables
     // dynamically generate Firebase data into a table
     // display a hiking gif?
+      var database = firebase.database();
 
+      // retrieve from Firebase
+      database.ref().on("value", function(instance) {
+          var newUser = instance.val().userName;
+          var newLocale = instance.val().userLocale;
+          var newEmail = instance.val().userEmail;
+      });
+      
 }); // end
